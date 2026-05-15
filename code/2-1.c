db1943a81ac0677069f55457ad25438507ddcb1a
권기범 <jchsoo@MacBookAir-2.local>
2026-05-15 09:30:46 +0900

    try1

---
+++ code/2-1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+enum State { IDLE, RUNNING, PAUSED, DONE};
+
+void print_state(enum State s) {
+    switch (s)
+    {
+    case IDLE:
+        printf("State: IDLS\n");
+        break;
+    case RUNNING:
+        printf("State: Running\n");
+    default: printf("Unknown state\n");
+    }
+}
+
+int main(void) {
+    print_state(RUNNING);
+    return 0;
+}
